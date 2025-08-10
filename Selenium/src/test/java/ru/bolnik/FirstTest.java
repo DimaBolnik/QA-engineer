ff4bff140416e6ff5c82582d0426862cfd87ca96
Dima Bolnik <dimabolnik@mail.ru>
2025-08-10 11:59:41 +0300

    Загрузка драйвера и тест

---
+++ Selenium/src/test/java/ru/bolnik/FirstTest.java
@@ -0,0 +1,24 @@
+package ru.bolnik;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FirstTest {
+
+    @Test
+    public void testSearchGoogle() {
+//        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com");
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+
+        driver.quit();
+    }
+}
