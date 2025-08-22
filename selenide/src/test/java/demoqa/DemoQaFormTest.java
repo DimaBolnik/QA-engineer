2cf79dbee7c901bba786b611380d0218bac12e7a
Dima Bolnik <dimabolnik@mail.ru>
2025-08-22 11:28:16 +0300

    Тест формы ввода на  demoQa 2.1

--- selenide/src/test/java/demoqa/DemoQaFormTest.java
+++ selenide/src/test/java/demoqa/DemoQaFormTest.java
@@ -1,23 +1,38 @@
+package demoqa;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DemoqaFormTest {
+public class DemoQaFormTest {
 
     @BeforeAll
     public static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        System.out.println("beforeEach");
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        System.out.println("afterEach");
     }
 
 
     @Test
     void fillPracticeForm() {
         // Открываем страницу
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
 
         // Вводим имя
         $("#firstName").setValue("Dmitry");
@@ -31,7 +46,8 @@ public class DemoqaFormTest {
         $("#userEmail").setValue("dmitry@example.com");
 
         // Выбираем пол (радио кнопка)
-        $("[for='gender-radio-1']").click();
+        $("#genterWrapper")
+                .$(byText("Male")).click();
         // $("[for='gender-radio-1']") — выбираем label по атрибуту for
         // .click() — клик по элементу
 
@@ -42,7 +58,12 @@ public class DemoqaFormTest {
         $("#dateOfBirthInput").click(); // открываем календарь
         $(".react-datepicker__month-select").selectOption("July"); // выбираем месяц
         $(".react-datepicker__year-select").selectOption("1990"); // выбираем год
-        $(".react-datepicker__day--010").click(); // выбираем день
+        //  $$(".react-datepicker__day--010")
+        //  .filter(not(cssClass("react-datepicker__day--outside-month")))
+        //  .first()
+        //  .click();
+        $(".react-datepicker__day--010:not(.react-datepicker__day--outside-month)").click();
+        // выбираем день
 
         // Выбираем предмет
         $("#subjectsInput").setValue("Maths").pressEnter();
@@ -71,5 +92,10 @@ public class DemoqaFormTest {
         $(".modal-content").shouldBe(visible);
         // shouldBe(visible) — проверяем, что элемент видимый на странице
         sleep(5000);
+    }
+
+    @Test
+    void test(){
+        Assertions.assertTrue(3 > 2);
     }
 }
